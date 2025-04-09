da678161252d915cf6f61c0132d500cbff717da0
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-04-09 19:36:26 +0700

    5. DataTable

--- Tuan_07_React_Again/src/pages/Home/Home.jsx
+++ Tuan_07_React_Again/src/pages/Home/Home.jsx
@@ -2,8 +2,8 @@ import OrdersTable from '../../components/Datatable/DataTable'
 function Home (){
     return(
         <>
-            {/* <OrdersTable></OrdersTable> */}
-            <h1>This is Home page</h1>
+            <OrdersTable></OrdersTable>
+            {/* <h1>This is Home page</h1> */}
         </>
     )
 }
