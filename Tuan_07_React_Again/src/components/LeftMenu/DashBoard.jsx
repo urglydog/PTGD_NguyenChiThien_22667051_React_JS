5849cf1b4746a9e7aa3c6010870365556da719e2
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-04-09 19:20:48 +0700

    2. isActive (NavLink React Router)

--- Tuan_07_React_Again/src/components/LeftMenu/DashBoard.jsx
+++ Tuan_07_React_Again/src/components/LeftMenu/DashBoard.jsx
@@ -16,38 +16,38 @@ function DashBoard() {
             <nav>
                 <ul>
                     <li
-                    //  className={location.pathname === path.dashboard ? 'active' : ''}
-                     >
+                        className={location.pathname === path.dashboard ? 'active' : ''}
+                    >
                         <img src={dashboardImg} alt="Dashboard" />
                         <Link to={path.dashboard}>Dashboard</Link>
                     </li>
                     <li
-                    //  className={location.pathname === path.project ? 'active' : ''}
-                     >
+                        className={location.pathname === path.project ? 'active' : ''}
+                    >
                         <img src={projectImg} alt="Projects" />
                         <Link to={path.project}>Projects</Link>
                     </li>
                     <li
-                    //  className={location.pathname === path.team ? 'active' : ''}
-                     >
+                        className={location.pathname === path.team ? 'active' : ''}
+                    >
                         <img src={teamImg} alt="Teams" />
                         <Link to={path.team}>Teams</Link>
                     </li>
                     <li
-                    //  className={location.pathname === path.analystic ? 'active' : ''}
-                     >
+                        className={location.pathname === path.analystic ? 'active' : ''}
+                    >
                         <img src={analyticImg} alt="Analytics" />
                         <Link to={path.analystic}>Analytics</Link>
                     </li>
                     <li
-                    //  className={location.pathname === path.messages ? 'active' : ''}
-                     >
+                        className={location.pathname === path.messages ? 'active' : ''}
+                    >
                         <img src={messImg} alt="Messages" />
                         <Link to={path.messages}>Messages</Link>
                     </li>
                     <li
-                    //  className={location.pathname === path.intergration ? 'active' : ''}
-                     >
+                        className={location.pathname === path.intergration ? 'active' : ''}
+                    >
                         <img src={intergrationImg} alt="Integrations" />
                         <Link to={path.intergration}>Integrations</Link>
                     </li>
